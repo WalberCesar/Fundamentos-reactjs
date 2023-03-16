85446c30ed4f742f35649845236d5284b9df9dc8
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-03-16 20:21:39 -0300

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -53,15 +53,18 @@ function App() {
 
       <div className={style.wrapper}>
         <Sidebar />
-        {posts.map((post) => {
-          return (
-            <Post
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
-            />
-          );
-        })}
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
       </div>
     </>
   );
